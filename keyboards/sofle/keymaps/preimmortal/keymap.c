084a8112756c4357b25879c4119e19dc42a9edea
preimmortal <preimmortal@gmail.com>
2024-08-15 19:50:55 -0700

    feat: change thumb keys on sofle

--- keyboards/sofle/keymaps/preimmortal/keymap.c
+++ keyboards/sofle/keymaps/preimmortal/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_LCTL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_MUTE, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_ESC,
-                 KC_COPY,KC_PSTE,KC_LGUI, TL_LOWR, KC_SPC,      KC_ENT,  TL_UPPR, KC_RALT, LALT(KC_BSPC), KC_RGUI
+               LCTL(KC_LALT),KC_LALT,KC_LGUI, TL_LOWR, KC_SPC,    KC_ENT, TL_UPPR, KC_RALT, LALT(KC_BSPC), KC_RGUI
 ),
 /*
  * COLEMAK
