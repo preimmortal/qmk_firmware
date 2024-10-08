a23927b792af543b1ba470bc0302da8092017fbd
preimmortal <preimmortal@gmail.com>
2024-10-08 02:13:45 -0700

    swap encoder to pgup and pgdn

--- keyboards/sofle/keymaps/preimmortal/keymap.c
+++ keyboards/sofle/keymaps/preimmortal/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT(
- KC_TILDE,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
+   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_LCTL,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,   KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_MUTE, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_ESC,
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,    KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_BSPC,
    KC_TAB,  KC_ESC,    KC_W, KC_MINS,  KC_EQL, KC_LBRC,                        KC_CIRC,  KC_ASTR, KC_LPRN, KC_RPRN, KC_AMPR,  KC_BSPC,
   KC_LCTL,    KC_A,    KC_S,    KC_D, KC_RBRC, KC_BSLS,                        KC_MINS,   KC_EQL, KC_LBRC, KC_RBRC, KC_PIPE, KC_GRAVE,
-  KC_LSFT, KC_SCLN, KC_QUOT,  KC_GRV, KC_COMM,  KC_DOT, _______,       _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS, KC_TILDE,
+  KC_LSFT, KC_SCLN, KC_QUOT, KC_SLSH, KC_COMM,  KC_DOT, _______,       _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS, KC_TILDE,
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 /* RAISE
