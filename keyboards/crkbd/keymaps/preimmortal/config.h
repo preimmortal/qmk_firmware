81d0a42ebf80d830e5a606afd56a72c317933753
preimmortal <preimmortal@gmail.com>
2026-03-28 23:42:20 -0700

    fix: add higher debounce time to crkbd

--- keyboards/crkbd/keymaps/preimmortal/config.h
+++ keyboards/crkbd/keymaps/preimmortal/config.h
@@ -28,6 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define DEBOUNCE 10
+
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
